2232755e08dc7758d3fb327d86937eacfcf218ef
Gennadiy <xenarki@gmail.com>
2022-05-13 15:53:27 +0700

    .

--- djangostud/main/views.py
+++ djangostud/main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4>Hello world</h4>")
-
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h3>About</h3>")
+    return render(request, 'main/about.html')
