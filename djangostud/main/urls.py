9017173c4aa1de3f041f75fa76d86723d5a5ae20
Home PC <xenarki@gmail.com>
2022-05-14 17:25:10 +0700

    bootstrap

--- djangostud/main/urls.py
+++ djangostud/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
-]
+    path('', views.index, name='home'),
+    path('about', views.about, name='about')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
