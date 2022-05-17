90e506117cc41595d6fcb495592e1c9513ce2ddd
Home PC <xenarki@gmail.com>
2022-05-18 00:15:24 +0700

    7

--- djangostud/news/urls.py
+++ djangostud/news/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
+    path('/create', views.create, name='create'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
