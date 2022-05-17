6de02c349f96f83feed1acdbeb513a8d7f0184a9
Gennadiy <xenarki@gmail.com>
2022-05-17 16:51:27 +0700

    1

--- djangostud/news/views.py
+++ djangostud/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-import http
+from .models import Articles
 
 # Create your views here.
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
